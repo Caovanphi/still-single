7ad3e4e87f7ab6c0cbeb58ad6a31371ae23e82d9
anh phi <106562909+Caovanphi@users.noreply.github.com>
2022-05-31 19:37:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ const CONFIG = {
     Lạc vào tim Quyết lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Làm người yêu tớ nha 🥰',
+    title: 'Lệ làm người yêu của quyết nha 🥰',
     desc: '',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Cho tớ biết lí do đi',
+    btnNo: 'Đéo :3',
+    question: 'Cho Quyết biết lí do đi',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'HiHi Quyết biết mà 🥰. Yêu Lệ nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, Quyết ra đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/re.nga.39794'
 }
