0e3ca72309d7bdbaab11efd3f3c6206766b78aea
anh phi <106562909+Caovanphi@users.noreply.github.com>
2022-05-31 21:53:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,19 +2,20 @@ const CONFIG = {
     titleWeb: "Love confess",
     introTitle: 'Who are you',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao lệ cứ đi nhầm đường
-    Lạc vào tim Quyết lẻ loi
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Lệ làm người yêu của quyết nha 🥰',
+    title: 'Làm người yêu của anh nha 🥰',
     desc: '',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Đéo :3',
-    question: 'Cho Quyết biết lí do đi',
+    btnNo: 'Không nha :3',
+    question: 'Cho anh biết lí do đi',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu',
-    mess: 'HiHi Quyết biết mà 🥰. Yêu Lệ nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, Quyết ra đón đi chơi nha.',
+    mess: 'HiHi anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, anh ra đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/re.nga.39794'
+    messLink: 'https://www.facebook.com(trang cá nhân facebook)
+   '
 }
